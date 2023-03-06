751ed756f8bb1ae0c14aff8d971954939e1f6574
mironshox2010 <mironshoxcodeschool@gmail.com>
2023-03-06 12:32:12 +0500

    Index05.py

--- Index05.py
+++ Index05.py
@@ -6,4 +6,17 @@ def main(s):
     Returns:
         int: answer
     """
-    return
+    s_count = 0
+    if s[0].isdigit():
+        s_count += 1
+    if s[1].isdigit():
+        s_count += 1
+    if s[2].isdigit():
+        s_count += 1
+    if s[3].isdigit():
+        s_count += 1
+    if s[4].isdigit():
+        s_count += 1
+    return s_count
+print(main("56567"))
+    
